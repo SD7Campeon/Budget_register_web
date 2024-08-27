6c705160c4be3a2a76227132c712e462e6c95cd2
Shishir Dua <103040543+SD7Campeon@users.noreply.github.com>
2024-08-27 14:52:52 +0530

    Create login.php

---
+++ application/views/auth/login.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="card mt-5">
+        <div class="card-body">
+            <h3 class="card-title">Login</h3>
+            <?php if ($this->session->flashdata('error')): ?>
+                <div class="alert alert-danger"><?php echo $this->session->flashdata('error'); ?></div>
+            <?php endif; ?>
+            <form action="<?php echo base_url('auth/login_action'); ?>" method="post">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" name="username">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
